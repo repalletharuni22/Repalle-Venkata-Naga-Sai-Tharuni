e89ff5f422f05272370ccc3b2677bafcc9d64dd6
repalletharuni22 <repalletharuni@gmail.com>
2026-09-22 12:03:50 +0530

    Fix formatting in prompt for side input

--- code.c
+++ code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     int side,area;
-    printf("Enter the side : ");
+    printf("Enter the side  : ");
     scanf("%d",&side);
     area=side*side;
     printf("The area of the square is : %d squarecm",area);
