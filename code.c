308be9238838d54736a9dacfff454d05e39e3aa1
repalletharuni22 <repalletharuni@gmail.com>
2026-09-22 11:53:29 +0530

    New commit

---
+++ code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int side,area;
+    printf("Enter the side : ");
+    scanf("%d",&side);
+    area=side*side;
+    printf("The area of the square is : %d squarecm",area);
+    return 0;
+}
